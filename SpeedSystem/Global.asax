5324ef14cc2a7866d650f1adf3fbe14e9a2592a5
Jaidson <jaidsondantas@gmail.com>
2019-02-02 16:25:18 -0200

    Adicionar arquivos de projeto.

---
+++ SpeedSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SpeedSystem.MvcApplication" Language="C#" %>
---
+++ SpeedSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SpeedSystem.Startup))]
+namespace SpeedSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
