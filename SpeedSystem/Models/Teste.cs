6dfa773cbb5d407121fb4d7ef1d5b423e3b6292b
Jaidson <jaidsondantas@gmail.com>
2019-02-03 19:39:59 -0200

    Scaffold Iniciado

---
+++ SpeedSystem/Models/Teste.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpeedSystem.Models
+{
+    public class Teste
+    {
+        public int TesteId { get; set; }
+        public string Name { get; set; }
+    }
+}
