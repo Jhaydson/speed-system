744cbe0dfce9e2fca85a49d149a7282cafe28ccf
jaidson <jaidsondantasgmail.com>
2019-02-04 17:55:24 -0200

    Controller criado para não aceitar dados duplicado e retornar a mensagem

---
+++ SpeedSystem/Models/Teste.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SpeedSystem.Models
+{
+    public class Teste
+    {
+        public int TesteId { get; set; }
+        [Required]
+        [Display(Name = "Departamentos")]
+        [MaxLength(50, ErrorMessage = "O Campo {0} recebe no máximo 50 caracteres")]
+        [Index("Department_Name_Index", IsUnique = true)]
+        public string Name { get; set; }
+    }
+}
