7f86fd29d48c46527de7e5bffcaa282cc361a0a8
jaidson <jaidsondantasgmail.com>
2019-02-04 15:44:25 -0200

    t4 list e t4 edit cadastradas

--- SpeedSystem/Models/Teste.cs
+++ SpeedSystem/Models/Teste.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Web;
 
 namespace SpeedSystem.Models
@@ -9,5 +10,8 @@ namespace SpeedSystem.Models
     {
         public int TesteId { get; set; }
         public string Name { get; set; }
+
+
+         
     }
 }
